3d124e04afec326aac0663c507e9d9a8b69f0edd
RMAbdullah6212 <abdullahaslam6212@gmail.com>
2026-06-30 20:30:43 +0500

    vercel settings

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://hospital-management-system-frontend-green.vercel.app/",],
+    credentials: true
+}
+));
+
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 
